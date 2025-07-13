b2f154d942a140f5bcb697dec739deb6088611d9
Naram99 <farkassebi@gmail.com>
2025-07-13 23:42:43 +0200

    light and dark color palette

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,15 +1,31 @@
 const colors: { [index: string]: ColorScheme } = {
     dark: {
-        mainColor: "",
-        secondaryColor: "",
-        mainBackground: "",
-        secondaryBackground: "",
+        mainColor: "#F73D93",
+        secondaryColor: "#E195AB",
+        mainBackground: "#413F42",
+        secondaryBackground: "#7F8487",
         mainHover: "",
         secondaryHover: "",
         title: "",
     },
-    light: {},
-    blue: {},
+    light: {
+        mainColor: "#EC7FA9",
+        secondaryColor: "#BE5985",
+        mainBackground: "#FFEDFA",
+        secondaryBackground: "#FFB8E0",
+        mainHover: "",
+        secondaryHover: "",
+        title: "",
+    },
+    blue: {
+        mainColor: "#",
+        secondaryColor: "#",
+        mainBackground: "#",
+        secondaryBackground: "#",
+        mainHover: "",
+        secondaryHover: "",
+        title: "",
+    },
 };
 
 export default colors;
