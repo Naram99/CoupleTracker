f3e33d3693e181a85c1ece35565f30ca475f769b
Naram99 <farkassebi@gmail.com>
2025-07-17 00:08:49 +0200

    settings input

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,11 +1,77 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+    Button,
+    StyleSheet,
+    Text,
+    TextInput,
+    TextInputComponent,
+    View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { Link } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Settings = () => {
+    const [username, setUsername] = useState<string | null>(null);
+    const [partnername, setPartnername] = useState<string | null>(null);
+    const [date, setDate] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function getUsernameFromStorage() {
+            try {
+                setUsername(await AsyncStorage.getItem("username"));
+            } catch {
+                setUsername(null);
+            }
+        }
+
+        async function getPartnernameFromStorage() {
+            try {
+                setPartnername(await AsyncStorage.getItem("partnername"));
+            } catch {
+                setPartnername(null);
+            }
+        }
+
+        async function getDateFromStorage() {
+            try {
+                setDate(await AsyncStorage.getItem("date"));
+            } catch {
+                setDate(null);
+            }
+        }
+
+        getUsernameFromStorage();
+        getPartnernameFromStorage();
+        getDateFromStorage();
+    }, []);
+
+    async function setUsernameToStorage() {
+        if (username) await AsyncStorage.setItem("username", username);
+    }
+
+    async function setPartnernameToStorage() {
+        if (partnername) await AsyncStorage.setItem("partnername", partnername);
+    }
+
+    async function setDateToStorage() {
+        if (date) await AsyncStorage.setItem("date", date);
+    }
+
+    function handleSubmit() {
+        setUsernameToStorage();
+        setPartnernameToStorage();
+        setDateToStorage();
+    }
+
     return (
         <View>
             <Text>Settings</Text>
+            <TextInput
+                value={username ?? ""}
+                style={styles.input}
+                onChangeText={setUsername}
+            />
+            <Button title="Save" onPress={handleSubmit} />
             <Link href={"/"} style={styles.link}>
                 Main page
             </Link>
@@ -19,4 +85,5 @@ const styles = StyleSheet.create({
     link: {
         textDecorationStyle: "solid",
     },
+    input: {},
 });
