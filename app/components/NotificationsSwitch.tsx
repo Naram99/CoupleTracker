7c661c850622f25b176f852228ed2108afb9772f
Naram99 <farkassebi@gmail.com>
2025-10-18 13:20:59 +0200

    console log removed

--- app/components/NotificationsSwitch.tsx
+++ app/components/NotificationsSwitch.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native'
-import React, { useEffect } from 'react'
-import SwitchInputField from './SwitchInputField'
-import { ColorScheme } from '../../constants/colors'
+import { StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import SwitchInputField from "./SwitchInputField";
+import { ColorScheme } from "../../constants/colors";
 import * as Notifications from "expo-notifications";
 
 const NotificationsSwitch = ({
@@ -10,25 +10,25 @@ const NotificationsSwitch = ({
     user,
     partner,
     date,
-    theme
+    theme,
 }: {
-    enabled: boolean,
-    onChange: () => void,
-    user: string | null,
-    partner: string | null,
-    date: Date
-    theme: ColorScheme
+    enabled: boolean;
+    onChange: () => void;
+    user: string | null;
+    partner: string | null;
+    date: Date;
+    theme: ColorScheme;
 }) => {
     useEffect(() => {
         const requestPermissions = async () => {
             const settings = await Notifications.requestPermissionsAsync();
-            if (
+            /* if (
                 settings.granted ||
                 settings.ios?.status ===
                     Notifications.IosAuthorizationStatus.PROVISIONAL
             ) {
                 console.log("Engedély megadva az értesítésekhez.");
-            }
+            } */
 
             Notifications.setNotificationHandler({
                 handleNotification: async () => ({
@@ -46,20 +46,32 @@ const NotificationsSwitch = ({
     }, [user, partner, date, enabled]);
 
     async function setupAllAlerts() {
-        const needed = await checkIfNotificationsAreScheduled()
+        const needed = await checkIfNotificationsAreScheduled();
 
         if (needed.days) {
-            const next100days = calcNext100Days(date)
-            const next100daysDate = new Date(date.getTime() + (next100days * 1000 * 60 * 60 * 24))
+            const next100days = calcNext100Days(date);
+            const next100daysDate = new Date(
+                date.getTime() + next100days * 1000 * 60 * 60 * 24
+            );
             // console.log(next100days, next100daysDate.toDateString())
-            await setupAlert(user, partner, next100daysDate, next100days, "days")
+            await setupAlert(
+                user,
+                partner,
+                next100daysDate,
+                next100days,
+                "days"
+            );
         }
-        
+
         if (needed.years) {
-            const nextYear = calcNextYear(date)
-            const nextYearDate = new Date(`${date.getFullYear() + nextYear}-${date.getMonth() + 1}-${date.getDate()}`)
+            const nextYear = calcNextYear(date);
+            const nextYearDate = new Date(
+                `${date.getFullYear() + nextYear}-${
+                    date.getMonth() + 1
+                }-${date.getDate()}`
+            );
             // console.log(nextYear, nextYearDate.toDateString())
-            await setupAlert(user, partner, nextYearDate, nextYear, "year")
+            await setupAlert(user, partner, nextYearDate, nextYear, "year");
         }
     }
 
@@ -78,7 +90,11 @@ const NotificationsSwitch = ({
         await Notifications.scheduleNotificationAsync({
             content: {
                 title: `${elapsed} ${timeType}!`,
-                body: `Hey ${user ?? "{user}"}, celebrate your ${elapsed} ${timeType} together with ${partner ?? "{partner}"}!`,
+                body: `Hey ${
+                    user ?? "{user}"
+                }, celebrate your ${elapsed} ${timeType} together with ${
+                    partner ?? "{partner}"
+                }!`,
             },
             trigger: {
                 date: triggerDate,
@@ -88,40 +104,47 @@ const NotificationsSwitch = ({
     }
 
     async function checkIfNotificationsAreScheduled() {
-        const needed = { years: true, days: true }
-        const scheduled = await Notifications.getAllScheduledNotificationsAsync()
+        const needed = { years: true, days: true };
+        const scheduled =
+            await Notifications.getAllScheduledNotificationsAsync();
 
-        scheduled.forEach(data => {
-            if (data.content.title?.includes("year")) needed.years = false
-            if (data.content.title?.includes("days")) needed.days = false
-        })
+        scheduled.forEach((data) => {
+            if (data.content.title?.includes("year")) needed.years = false;
+            if (data.content.title?.includes("days")) needed.days = false;
+        });
 
-        return needed
+        return needed;
     }
 
     function calcNext100Days(date: Date): number {
-        const currDays = Math.floor((new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24))
-        return (Math.floor(currDays / 100) + 1) * 100
+        const currDays = Math.floor(
+            (new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24)
+        );
+        return (Math.floor(currDays / 100) + 1) * 100;
     }
 
     function calcNextYear(date: Date): number {
-        const currYear = new Date().getFullYear()
-        const givenYear = date.getFullYear()
-        const thisYearDate = new Date(`${currYear}-${date.getMonth() + 1}-${date.getDate()}`)
-        const yearDiff = currYear - givenYear
-        return thisYearDate.getTime() < new Date().getTime() ? yearDiff + 1 : yearDiff
+        const currYear = new Date().getFullYear();
+        const givenYear = date.getFullYear();
+        const thisYearDate = new Date(
+            `${currYear}-${date.getMonth() + 1}-${date.getDate()}`
+        );
+        const yearDiff = currYear - givenYear;
+        return thisYearDate.getTime() < new Date().getTime()
+            ? yearDiff + 1
+            : yearDiff;
     }
 
     return (
-        <SwitchInputField 
-            label='Enable notifications:'
+        <SwitchInputField
+            label="Enable notifications:"
             value={enabled}
             onChangeValue={onChange}
             theme={theme}
         />
-    )
-}
+    );
+};
 
-export default NotificationsSwitch
+export default NotificationsSwitch;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
